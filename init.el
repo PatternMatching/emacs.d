5acc1d446d11e1ac067ff2bd33198b4aa94cafde
PatternMatching <phennings@gmail.com>
2014-10-30 16:15:44 -0400

    Changed MELPA link per emacsworld

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
 (package-initialize)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")))
 
 ;; -----------------------------------
 ;; Org-Mode
