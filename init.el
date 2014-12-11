e8cf80970f6f85b7bacdc1e6b98d1a8235b50fc0
PatternMatching <phennings@gmail.com>
2014-12-11 18:02:05 -0500

    Made LaTeX its own init file

--- init.el
+++ init.el
@@ -46,16 +46,7 @@
 ;; -----------------------------------
 ;; LaTeX
 ;; -----------------------------------
-
-;; AucTex
-(require 'tex)
-(require 'tex-site)
-(require 'font-latex)
-(require 'preview)
-(require 'auto-complete-auctex)
-
-(add-hook 'LaTeX-mode-hook (lambda ()
-			     (TeX-global-PDF-mode t)))
+(require 'init-auctex)
 
 ;; Load MATLAB source files in Octave mode instead of Obj-C
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
---
+++ lisp/init-auctex.el
@@ -0,0 +1,17 @@
+
+;; AucTeX
+(require 'tex)
+(require 'tex-site)
+(require 'font-latex)
+(require 'preview)
+
+;; Auto-Complete for AucTeX
+(require 'auto-complete-auctex)
+
+;; Hooks
+;; ---------
+;; Want to auto fill the document and auto-compile to PDF
+(add-hook 'LaTeX-mode-hook (lambda ()
+			     'turn-on-auto-fill			 
+			     (TeX-global-PDF-mode t)))
+
