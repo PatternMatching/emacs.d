eb64c41bb9e71c2d49440268ad8e52da60d4c0d2
PatternMatching <E18349@exelonds.com>
2016-10-28 14:08:25 -0400

    Turns out eshell has its own path variable and Emacs uses exec-path

--- lisp/init-setpy.el
+++ lisp/init-setpy.el
@@ -6,12 +6,12 @@
 
 (defun setpy--in-stuff-to-remove(x)
   ""
-  (let ((stuff-to-remove '("C:\\WinPython-2.7.10.3-64"
-			   "C:\\WinPython-2.7.10.3-64\\python-2.7.10.amd64"
-			   "C:\\WinPython-2.7.10.3-64\\python-2.7.10.amd64\\scripts"
-			   "C:\\WinPython-2.7.10.3"
-			   "C:\\WinPython-2.7.10.3\\python-2.7.10"
-			   "C:\\WinPython-2.7.10.3\\python-2.7.10\\scripts")))
+  (let ((stuff-to-remove '("C:/WinPython-2.7.10.3-64"
+			   "C:/WinPython-2.7.10.3-64/python-2.7.10.amd64"
+			   "C:/WinPython-2.7.10.3-64/python-2.7.10.amd64/scripts"
+			   "C:/WinPython-2.7.10.3"
+			   "C:/WinPython-2.7.10.3/python-2.7.10"
+			   "C:/WinPython-2.7.10.3/python-2.7.10/scripts")))
     (member x stuff-to-remove)))
 
 (defun setpy--set-path (bit-arch)
@@ -21,18 +21,20 @@
 	 (py-strs '((64 . "python-2.7.10.amd64")
 		    (32 . "python-2.7.10")))
 	 (path (remove-if #'setpy--in-stuff-to-remove
-			  (split-string (getenv "PATH") ";")))
+			  exec-path))
 	 (winpy-str (cdr (assoc bit-arch winpy-strs)))
 	 (py-str (cdr (assoc bit-arch py-strs)))
 	 (new-path-entries (list
-			    (concat "C:\\" winpy-str)
-			    (concat "C:\\" winpy-str "\\" py-str)
-			    (concat "C:\\" winpy-str "\\" py-str "\\scripts"))))
-      (if (every #'file-accessible-directory-p new-path-entries)
-	  (message "PATH environment variable updated to: %s"
-	    (setenv "PATH" (mapconcat 'identity
-				      (append new-path-entries path) ";")))
-	(error "The WinPython directories don't exist"))))
+			    (concat "C:/" winpy-str)
+			    (concat "C:/" winpy-str "/" py-str)
+			    (concat "C:/" winpy-str "/" py-str "/scripts"))))
+    (if (every #'file-accessible-directory-p new-path-entries)
+	(setq exec-path (append new-path-entries path)
+	      eshell-path-env (mapconcat 'identity
+					 (append new-path-entries
+						 path
+						 (list exec-directory)) ";"))
+      (error "The WinPython directories don't exist"))))
 
 (defun setpy--set-py-path (new-py-path)
   ""
@@ -46,7 +48,7 @@
   (interactive
    (list
     (string-to-number (read-from-minibuffer "Bit-Architecture: " "64"))
-    (read-from-minibuffer "PYTHONPATH: ")))
+    (read-from-minibuffer "PYTHONPATH: " (getenv "PYTHONPATH"))))
     (if pythonpath
 	(progn
 	  (setpy--set-py-path pythonpath)
