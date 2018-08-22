8653459a4615253057b3f87d463e765de0deebc2
PatternMatching <E18349@exelonds.com>
2018-08-22 14:46:07 -0400

    Adding support for msys64 packages to enable use of GNU tools on windows

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -3,9 +3,11 @@
 (require 'magit)
 
 ;;; Code:
-(when (system-is-windows) (setenv "SSH_ASKPASS" "git-gui--askpass"))
+(when (system-is-windows)
+  (setenv "SSH_ASKPASS" "git-gui--askpass"))
 
-(when (system-is-windows) (require 'ssh-agency))
+(when (system-is-windows)
+  (require 'ssh-agency))
 
 (provide 'init-git)
 ;;; init-git.el ends here
--- lisp/init-path.el
+++ lisp/init-path.el
@@ -1,10 +1,15 @@
 ;; init-path.el
+
+;;; Code:
 (if (system-is-windows)
-    (progn
-      ;; Prepend current execution path with GNU tools
-      (setenv "PATH"
-	      (concat
-	       "C:\\MinGW\\msys\\1.0\\bin" ";"
-	       (getenv "PATH")))))
+    (let ((msys-bin-dir-esc "C:\\msys64\\usr\\bin")
+	  (msys-bin-dir '("c:/msys64/usr/bin")))
+      (progn
+	;; Prepend current execution path with GNU tools
+	(setenv "PATH"
+		(concat
+	         msys-bin-dir-esc ";"
+		 (getenv "PATH")))
+	(setq exec-path (append exec-path msys-bin-dir)))))
 
 (provide 'init-path)
