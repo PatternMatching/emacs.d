01fae48d7cdf5a33bb7a7ec4479ea854f64b00cc
Peter Hennings <phennings@Trailstonellc.com>
2021-01-28 08:58:58 -0600

    Deleting Marmalade

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -4,7 +4,6 @@
 ;; ELPA
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
