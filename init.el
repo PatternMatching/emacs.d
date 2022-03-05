b83d8e4bf8e487cfa152ae0b0ff776cedd613631
Peter Hennings <hennip@entergrid-laptop.local>
2022-03-05 11:05:57 -0600

    Not really sure what changed...

--- init.el
+++ init.el
@@ -75,8 +75,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (flycheck magit-svn pipenv json-reformat async ctable epl fsm pkg-info popup seq offlineimap jabber epc auctex))))
+   '(flycheck magit-svn pipenv json-reformat async ctable epl fsm pkg-info popup seq offlineimap jabber epc auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
