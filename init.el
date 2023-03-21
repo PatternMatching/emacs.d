1014f61268fee15ef2dca22b27a5da9fc50272f7
phennings <phennings@roscommonanalytics.com>
2023-03-21 13:53:21 -0500

    Added vterm

--- init.el
+++ init.el
@@ -75,7 +75,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exec-path-from-shell flycheck magit-svn pipenv json-reformat async ctable epl fsm pkg-info popup seq offlineimap jabber epc auctex)))
+   '(vterm exec-path-from-shell flycheck magit-svn pipenv json-reformat async ctable epl fsm pkg-info popup seq offlineimap jabber epc auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
