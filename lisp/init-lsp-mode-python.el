ffe4dd5324f3955b7dc30524714bc3200781d6ad
Peter Hennings <hennip@Peters-MacBook-Pro.local>
2024-07-03 14:58:31 -0500

    Adding configuration to run pytest via dap-mode

--- lisp/init-lsp-mode-python.el
+++ lisp/init-lsp-mode-python.el
@@ -9,7 +9,7 @@
   :commands lsp
   :config
   (setq lsp-restart 'auto-restart)
-  (setq read-process-output-max (* 1024 1024))
+  (setq read-process-output-max (* 1024 1024)))
 
 (use-package lsp-pyright
   :hook (python-mode . (lambda()
@@ -18,6 +18,14 @@
 
 (use-package lsp-ui
   :commands lsp-ui-mode)
+
+(use-package dap-mode
+  :after lsp-mode
+  :commands dap-debug
+  :hook ((python-mode . dap-ui-mode) (python-mode . dap-mode))
+  :config
+  (require 'dap-python)
+  (setq dap-python-debugger 'debugpy))
 
 (provide 'init-lsp-mode-python)
 ;;; init-lsp-mode-python.el ends here
