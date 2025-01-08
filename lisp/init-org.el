25325be42181cd1a844bf7fad2db1cd719b10344
Peter Hennings <hennip@Peters-MacBook-Pro.local>
2025-01-08 16:48:52 -0600

    Removing dependency on org-journal and adding visual niceties to org-mode-hook

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,15 +1,7 @@
 (require 'platform)
 
-(setq org-journal-install-directory "~/.emacs.d/lisp/org-journal/")
-(add-to-list 'load-path org-journal-install-directory)
-(require 'org-journal)
-
-(cond
- ((system-is-windows)
-  (setq org-journal-dir "~/journal/"))
- ((system-is-mac)
-  (setq org-journal-dir "~/Documents/journal/"))
- ((system-is-linux)
-  (setq org-journal-dir "~/journal/")))
+(add-hook `org-mode-hook (lambda()
+			   (visual-line-mode)
+			   (org-indent-mode)))
 
 (provide 'init-org)
